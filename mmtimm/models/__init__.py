ab6e2ec64e79ef57e8a03dc08f1f329521fc7db8
Rist115 <to78314910@gmail.com>
2021-09-24 18:57:20 +0900

    add cvt

--- mmtimm/models/__init__.py
+++ mmtimm/models/__init__.py
@@ -1,2 +1,4 @@
 # flake8: noqa:F401
+from .cvt import (cvt_13_224, cvt_13_384, cvt_13_384_22k, cvt_21_224,
+                  cvt_21_384, cvt_21_384_22k, cvt_w24)
 from .vip import vip_l7, vip_m7, vip_s7, vip_s14
