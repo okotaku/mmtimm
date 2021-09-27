6a251f0194dce59c777b2f9c5cf1b15457a00d39
Rist115 <to78314910@gmail.com>
2021-09-27 16:04:19 +0900

    add irpe

--- mmtimm/models/__init__.py
+++ mmtimm/models/__init__.py
@@ -4,6 +4,8 @@ from .cvt import (cvt_13_224, cvt_13_384, cvt_13_384_22k, cvt_21_224,
                   cvt_21_384, cvt_21_384_22k, cvt_w24)
 from .cyclemlp import (cyclemlp_b1, cyclemlp_b2, cyclemlp_b3, cyclemlp_b4,
                        cyclemlp_b5)
+from .irpe import (deit_base_patch16_224_ctx_product_50_shared_qkv,
+                   deit_small_patch16_224_ctx_product_50_shared_qkv)
 from .shuffle_transformer import (shuffle_vit_base_patch4_window7_224,
                                   shuffle_vit_small_patch4_window7_224,
                                   shuffle_vit_tiny_patch4_window7_224)
